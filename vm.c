470f8df837c9ae1a5ab9743d09b95dc073932c17
Allen Pereira <allenpereira17@gmail.com>
2021-01-24 21:27:25 -0500

    set up while loop

--- vm.c
+++ vm.c
@@ -18,18 +18,20 @@ typedef struct instruction
 int main(int argc, char **argv)
 {
   FILE * file_ptr = fopen(argv[1], "r");
-  instruction instruction_array[MAX_CODE_LENGTH];
+  instruction text[MAX_CODE_LENGTH];
+	int stack[MAX_STACK_HEIGHT];
   int count = 0;
 	int sp = -1;
 	int bp = 0;
 	int pc = 0;
 	instruction *ir = NULL;
 	int last_index = -1;
+	int halt = 1;
 
 
   while (feof(file_ptr) == 0)
   {
-    fscanf(file_ptr, "%d %d %d", &instruction_array[count].opcode, &instruction_array[count].l, &instruction_array[count].m);
+    fscanf(file_ptr, "%d %d %d", &text[count].opcode, &text[count].l, &text[count].m);
     count++;
   }
 
@@ -38,5 +40,16 @@ int main(int argc, char **argv)
 	printf("\t\t PC\tBP\tSP\tstack\n");
 	printf("Initial values:  %2d\t%2d\t%2d\n",pc,bp,sp);
 
-	
+	while (halt)
+	{
+		switch (text[pc].opcode)
+		{
+
+
+
+
+
+		}
+		pc++;
+	}
 }
